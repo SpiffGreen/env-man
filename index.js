c006b360c90fa0649249161ed19d2a927a305e04
SpiffGreen <spiffjekeygreen@gmail.com>
2021-07-20 13:13:43 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,52 @@
+/**
+ * @package env-manager
+ * @author  Spiff Jekey-Green
+ * @version 0.0.0
+ */
+
+const path = require("path");
+const fs = require("fs");
+
+class EnvManger {
+    constuctor() {
+        this.set = this.set.bind(this);
+        this.get = this.get.bind(this);
+        this.loadFromFile = this.loadFromFile.bind(this);
+        this.config = this.config.bind(this);
+    }
+
+    config(envFolderPath) {
+        this.loadFromFile(path.join(envFolderPath, '.env'));
+    }
+
+    set(key, value) {
+        process.env[key] = value;
+    }
+
+    get(key) {
+        return process.env[key];
+    }
+
+    loadFromFile(pathToEnvFile) {
+        try {
+            let content = fs.readFileSync(pathToEnvFile, {encoding: "utf-8"});
+            content = content.split("\n");
+            content.forEach(i => {
+                const [key, value] = i.split("=");
+                process.env[key.trim()] = value.trim();
+            });
+        } catch {
+            return;
+        }
+    }
+}
+
+module.exports = new EnvManger();
+
+/*
+Sample usage
+require("./index").config(__dirname);
+
+console.log(process.env.SECRET);
+console.log(process.env.DB_URL);
+*/
